23e5430f9da46579e28634371806639f3567a77e
= <=>
2017-02-05 09:46:33 -0500

    passport init

--- app.js
+++ app.js
@@ -20,41 +20,49 @@ var users = require('./routes/users');
 var app = express();
 
 //View Engine
-app.set('views',path.join(__dirname,'views'));
-app.engine('handlebars',exphbs({defaultLayout:'layout'}));
-app.set('view engine','handlebars');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'layout'
+}));
+app.set('view engine', 'handlebars');
 
 //BodyParser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser);
 
 //Set static folder
-app.set(express.static(path.join(__dirname,'public'))); 
+app.set(express.static(path.join(__dirname, 'public')));
 
 //Express session
 app.use(session({
-    secret:'secret',
-    saveUninitialized:true,
-    resave:true
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
 }));
+
+//Passport Init
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Express Validator
 app.use({
-    errorFormatter:function(param,msg,value){
-        var namespace = param.split('.')
-        , root = namespace.shift()
-        , formParm = root;
-    
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParm = root;
 
-    while(namespace.length){
-        formParm += '['+ namespace.shift() +']'
-    }
-    return {
-        param formParm,
-        msg:msg,
-        value:value
-    }
+
+        while (namespace.length) {
+            formParm += '[' + namespace.shift() + ']'
+        }
+        return {
+            para: formParm,
+            msg: msg,
+            value: value
+        }
     }
 });
 
@@ -62,20 +70,20 @@ app.use({
 app.use(flash());
 
 //Global Vars
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
 
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
 
-app.use('/',routes);
-app.use('/users',users);
+    app.use('/', routes);
+    app.use('/users', users);
 
-//Set Port
-app.set('port',(process.env.PORT || 3000));
+    //Set Port
+    app.set('port', (process.env.PORT || 3000));
 
-app.listen(app.get('port'),function(){
-    console.log('Server started on port ' + app.get('port'));
-});
+    app.listen(app.get('port'), function () {
+        console.log('Server started on port ' + app.get('port'));
+    });
 
 });
