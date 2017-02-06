e377d85f69aeace2a41ec8ca049476cd9010c31e
= <=>
2017-02-06 14:32:24 -0500

    Login functionality

--- models/user.js
+++ models/user.js
@@ -19,14 +19,34 @@ var UserSchema = mongoose.Schema({
     }
 });
 
-var User = module.exports = mongoose.model('User',UserSchema);
+var User = module.exports = mongoose.model('User', UserSchema);
 
-module.exports.createUser = function(newUser, callback){
-    bcrypt.genSalt(10,function(err,salt){
-        bcrypt.hash(newUser.password,salt, function(err,hash){
+module.exports.createUser = function (newUser, callback) {
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(newUser.password, salt, function (err, hash) {
             //Store hash in your password DB.
             newUser.password = hash;
             newUser.save(callback);
         })
     })
+}
+
+module.exports.getUserByUsername = function (username, callback) {
+    var query = {
+        username: username
+    };
+
+    User.findOne(query, callback);
+}
+
+module.exports.comparePassword = function (candidatePassword, hash, callback) {
+    bcrypt.compare(candidatePassword, hash, function (err, isMatch) {
+        if (err) throw err;
+        callback(null, isMatch);
+    });
+
+}
+
+module.exports.getUserById = function (id, callback) {
+    User.findById(id, callback);
 }
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user')
 //Register
@@ -27,23 +29,23 @@ router.post('/register', function (req, res) {
     var errors = req.validationErrors();
 
     if (errors) {
-        res.render('register',{
-            errors:errors
+        res.render('register', {
+            errors: errors
         })
     } else {
         var newUser = new User({
-            name:name,
-            email:email,
-            username:username,
-            password:password
+            name: name,
+            email: email,
+            username: username,
+            password: password
         });
 
-        User.createUser(newUser,function(err,user){
-            if(err) throw err;
+        User.createUser(newUser, function (err, user) {
+            if (err) throw err;
             console.log(user);
         });
 
-        req.flash('success_msg','You are registered and can now login');
+        req.flash('success_msg', 'You are registered and can now login');
 
         res.redirect('/users/login');
     }
@@ -55,5 +57,47 @@ router.get('/login', function (req, res) {
     res.render('login');
 });
 
+passport.use(new LocalStrategy(
+    function(username, password,done){
+      User.getUserByUsername(username, function(err,user){
+            if(err) throw err;
+
+            if(!user){
+                return done(null, false, {message:'Unknown User'});
+            }
+
+            User.comparePassword(password, user.password,function(err,isMatch){
+                if(err) throw err;
+
+                if(isMatch)
+                {
+                    return done(null,user)
+                }else
+                {
+                    return done(null,false,{message:'Invalid password'});
+                }
+            })
+      });
+    }
+));
+
+passport.serializeUser(function(user,done){
+    done(null,user.id);
+});
+
+passport.deserializeUser(function(id,done){
+    User.getUserById(id,function(err,user){
+        done(err,user);
+    })
+});
+
+router.post('/login',
+    passport.authenticate('local',{sucessRedirect:'/', failureRedirect:'/users/login', failureFlash:true}),
+    function (req, res) {
+        //If this function gets called, authentication was successful.
+        //req.user contains the authenticated user
+        
+        res.redirect('/')
+    });
 
 module.exports = router;
