7f5e5cf7012345aa789c15b33e1537e6b4c3b8a5
= <=>
2017-02-06 07:58:32 -0500

    added models and views.  Formattted app.js

--- app.js
+++ app.js
@@ -10,80 +10,78 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost/loginapp');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-//Init loginapp
+// Init App
 var app = express();
 
-//View Engine
+// View Engine
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({
-    defaultLayout: 'layout'
-}));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
 app.set('view engine', 'handlebars');
 
-//BodyParser Middleware
+// BodyParser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(cookieParser);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-//Set static folder
-app.set(express.static(path.join(__dirname, 'public')));
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-//Express session
+// Express Session
 app.use(session({
     secret: 'secret',
     saveUninitialized: true,
     resave: true
 }));
 
-//Passport Init
+// Passport init
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Express Validator
-app.use({
-    errorFormatter: function (param, msg, value) {
-        var namespace = param.split('.'),
-            root = namespace.shift(),
-            formParm = root;
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
 
-
-        while (namespace.length) {
-            formParm += '[' + namespace.shift() + ']'
-        }
-        return {
-            para: formParm,
-            msg: msg,
-            value: value
-        }
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
     }
-});
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
-//Connect flash
+// Connect Flash
 app.use(flash());
 
-//Global Vars
+// Global Vars
 app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
 
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
 
-    app.use('/', routes);
-    app.use('/users', users);
 
-    //Set Port
-    app.set('port', (process.env.PORT || 3000));
+app.use('/', routes);
+app.use('/users', users);
 
-    app.listen(app.get('port'), function () {
-        console.log('Server started on port ' + app.get('port'));
-    });
+// Set Port
+app.set('port', (process.env.PORT || 3000));
 
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
 });
